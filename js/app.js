a4a66b48642ec7afdb18e9f9835ab55765eb8b0c
fredsonPOSantos <son.magometal@gmail.com>
2024-11-02 00:28:00 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = '35.160.120.126/api'; 'https://138.204.143.189:8182/api/' ;
+const API_URL = 'https://api-agendamento-idb2.onrender.com/api/' ;
 
 // Função para verificar a URL atual
 function isDashboardPage() {
