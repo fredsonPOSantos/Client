02350f79ef8111682205902ac22a1adb054f35ae
fredsonPOSantos <son.magometal@gmail.com>
2024-11-02 20:17:53 -0300

    Update admin-app.js

--- js/admin-app.js
+++ js/admin-app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api-agendamento-idb2.onrender.com/api/appointments' ; 
+const API_URL = 'https://api-agendamento-idb2.onrender.com' ; 
 // Função para fazer login
 async function loginUser(event) {
     event.preventDefault();
