f0903560757ed41afe20c2febf3f218995a01961
fredsonPOSantos <son.magometal@gmail.com>
2024-11-11 00:08:15 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -113,7 +113,8 @@ async function loadAppointments(username) {
                     <div>
                         <p>${appointment.serviceType} - ${formattedDateTime} UTC</p>
                         <button onclick="rescheduleAppointment('${appointment._id}')">Remarcar</button>
-                        <button onclick="cancelAppointment('${appointment._id}')">Cancelar</button>
+                        <button onclick="cancelAppointment('${appointment._id}')" style="background-color: rgb(112, 22, 22); color: white;">Cancelar</button></div>
+
                     </div>
                 `;
             }).join('');
